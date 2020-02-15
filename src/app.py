707951dbfa3a7f31c49a72ad632e3342cb12b564
Andy Wang <andywang.16@gmail.com>
2020-02-15 18:23:50 +0000

    App is now working

--- src/app.py
+++ src/app.py
@@ -1,10 +1,11 @@
+import itertools
 import numpy as np
 from pathlib import Path
 
 
 class Scrabble:
     def match(self, chars):  # todo
-        self.words
+        self.words.cont
         matches = np.array()
         return matches
 
@@ -14,12 +15,35 @@ class Scrabble:
         self.length = self.words.size
 
 
-dict = Scrabble('./dictionary/scrabble_words.txt')
+def str_to_array(string):
+    char_array = np.array([])
+    for char in string:
+        char_array = np.append(char_array, char)
+    return char_array
+
+
+def candidate_list(input_array):
+    candidates = np.array([])
+    for i in range(input_array.size):
+        perms = itertools.permutations(input_array, i + 1)
+        for p in perms:
+            candidate = ''
+            for char in p:
+                candidate += char
+            candidates = np.append(candidates, candidate)
+    return np.unique(candidates)
+
+
+dict = Scrabble('./dictionary/scrabble_words_collins_2019.txt')
 print('Solving using a dictionary of ' + str(dict.length) + ' words.')
 
-while(True):
-    input_chars = input('Available characters? ').lower()
-    matches = dict.match(input_chars)
-    for i in matches.size:
-        print(matches[i])
-    input('Press enter to search again.')
+while(__name__ == '__main__'):
+    input_string = input('Available characters? ').upper()
+    input_array = str_to_array(input_string)
+
+    candidates = candidate_list(input_array)
+
+    match_array = np.isin(candidates, dict.words)
+    for i in range(match_array.size):
+        if match_array[i]:
+            print(candidates[i])
