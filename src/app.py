2180294bb023f2662a45441d89a2b385207e1a6e
Andy Wang <andywang.16@gmail.com>
2020-02-15 17:02:51 +0000

    WIP

--- src/app.py
+++ src/app.py
@@ -1,16 +1,25 @@
-import os
+import numpy as np
 from pathlib import Path
 
-os.chdir('./src')
+
+class Scrabble:
+    def match(self, chars):  # todo
+        self.words
+        matches = np.array()
+        return matches
+
+    def __init__(self, file_path):
+        dict_file = open(Path(file_path))
+        self.words = np.array(dict_file.read().split())
+        self.length = self.words.size
 
 
-def import_dictionary(file_path):
-    dict_file = open(Path(file_path))
-    dictionary = list(dict_file.read().split())
-    return dictionary
-
-
-dict = import_dictionary('./dictionary/scrabble_words.txt')
+dict = Scrabble('./dictionary/scrabble_words.txt')
+print('Solving using a dictionary of ' + str(dict.length) + ' words.')
 
 while(True):
-    print(dict)
+    input_chars = input('Available characters? ').lower()
+    matches = dict.match(input_chars)
+    for i in matches.size:
+        print(matches[i])
+    input('Press enter to search again.')
