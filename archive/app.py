47ca009b9234d82af2c5c846c00836707686eed2
Andy Wang <andywang.16@gmail.com>
2020-02-15 14:44:36 +0000

    Added start of new app

---
+++ archive/app.py
@@ -0,0 +1,40 @@
+import pandas as pd
+
+
+def alphasort(input):
+    output = ""
+    for char in sorted(input):
+        output += char
+    return output
+
+
+with open('words_alpha.txt') as dict_file:
+    words = list(dict_file.read().split())
+sorted_words = []
+for i in range(len(words)):
+    sorted_words.append(alphasort(words[i]))
+    print(words[i], sorted_words[i])
+print(sorted_words)
+
+
+# Load dictionary
+
+# words = pd.read_csv("words_alpha.csv")
+# words.columns = ["words"]
+# sorted_list = []
+# for i in range(len(words)):
+#     sorted_list.append(alphasort(words.iloc[i][0]))
+# words["alphasort"] = sorted_list
+
+# print(words)
+
+
+# Main loop
+while(True):
+    letters = input("Enter letters: ")
+
+    # Validation stage
+    if letters.isalpha() == False:
+        print("Invalid input.")
+    else:
+        print(words[10:15])
--- src/app.py
+++ src/app.py
@@ -1,43 +1,16 @@
-## Dictionary from https://github.com/dwyl/english-words ####
+import os
+from pathlib import Path
 
-import pandas as pd
-
-def alphasort(input):
-    output = ""
-    for char in sorted(input):
-        output += char
-    return output
-
-with open('words_alpha.txt') as dict_file:
-    words = list(dict_file.read().split())
-sorted_words = []
-for i in range(len(words)):
-    sorted_words.append(alphasort(words[i]))
-    print(words[i], sorted_words[i])
-print(sorted_words)
+os.chdir('./src')
 
 
-
-# Load dictionary
-
-# words = pd.read_csv("words_alpha.csv")
-# words.columns = ["words"]
-# sorted_list = []
-# for i in range(len(words)):
-#     sorted_list.append(alphasort(words.iloc[i][0]))
-# words["alphasort"] = sorted_list
-
-# print(words)
-
-    
+def import_dictionary(file_path):
+    dict_file = open(Path(file_path))
+    dictionary = list(dict_file.read().split())
+    return dictionary
 
 
-# Main loop
+dict = import_dictionary('./dictionary/scrabble_words.txt')
+
 while(True):
-    letters = input("Enter letters: ")
-
-    # Validation stage
-    if letters.isalpha() == False:
-        print("Invalid input.")
-    else:
-        print(words[10:15])
+    print(dict)
